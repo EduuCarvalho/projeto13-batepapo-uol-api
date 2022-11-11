449b96eb6652757c8d94e4e2a56df0f437a29397
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-11-11 16:16:46 -0300

    falta criar parametros de validação

--- src/app.js
+++ src/app.js
@@ -2,12 +2,25 @@ import express from 'express';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import dayjs from 'dayjs';
+import joi from 'joi';
+import cors from 'cors';
 
+
+const participantsSchema = joi.object({
+    name:joi.string().required()
+})
+
+const messagesSchema = joi.object({
+    to: joi.string().required(),
+    text:joi.string().required(),
+    type:joi.string().required(),
+})
 
 
 const app = express();
 
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 
 const mongoClient = new MongoClient("mongodb://localhost:27017")
@@ -20,11 +33,14 @@ try {
     console.log("error mongodb", err);
 }
 
-
+//TUdo certo com o get /participants
 app.get("/participants", async (req,res)=>{
 
     try{
-    const participants  = await db.collection("participants").find().toArray();
+    const participants  = await db
+    .collection("participants")
+    .find()
+    .toArray();
     res.send(participants)
     } catch (err){
         console.log(err);
@@ -32,9 +48,17 @@ app.get("/participants", async (req,res)=>{
     }
 });
 
-
+//TUdo certo com o post /participants
 app.post("/participants", async (req, res) => {
-    const name = req.body;
+    const {name}= req.body;
+
+    const validation = participantsSchema.validate(req.body,{ abortEarly:false});
+
+    if (validation.error){
+        const errors = validation.error.details.map((detail)=> detail.message);
+        res.send(errors);
+        return
+    }
 
     try {
         await db.collection("participants").insertOne({
@@ -43,18 +67,23 @@ app.post("/participants", async (req, res) => {
         });
         res.status(201).send("Participante postado");
     } catch (err) {
-        res.status(500).send(err);
+        res.status(422).send(err);
     }
 });
 
 
 app.get("/messages", async (req,res)=>{
     const limit = parseInt(req.query.limit);
+    console.log(limit)
     const user= req.headers.user;
 
+
     try{
-        const messages = await db.collection("message").find().toArray();
-        res.send(messages)
+        const messages = await db
+        .collection("message")
+        .find()
+        .toArray();
+        res.send(messages.slice(-limit))
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
@@ -64,9 +93,23 @@ app.get("/messages", async (req,res)=>{
 
 
 app.post("/messages", async (req,res)=>{
-    const {to, text, type} = req.body;
+    const {to,text,type} = req.body;
     const from = req.headers.user;
-    const time = dayjs().format('HH:MM:SS')
+    const time = dayjs().format('HH:mm:ss')
+    const validation = messagesSchema.validate(req.body,{ avortEarly: false});
+
+    if (validation.error){
+        const errors=validation.error.details.map((detail)=>detail.message);
+        res.send(errors);
+        return;
+    }
+   const userVerify= await db.collection('participants').find({name:from}).toArray();
+   console.log(userVerify)
+
+    if (userVerify.length ===0 ){
+        res.sendStatus(404);
+        return;
+    }
 
     try{
         await db.collection('message').insert({
